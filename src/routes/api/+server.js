96faff7e5279515db7a98f0eba373772ed08fbbb
Aradhya Tiwari <aradhya.prad@gmail.com>
2022-10-26 14:43:14 +0530

    again tried for file writing changes

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -1,11 +1,11 @@
 //import * as fs from 'fs'
-// import * as fs from 'fs';
+import * as fs from 'fs';
 
 export function GET({ url }) {
-    //     const fil = url.searchParams.get('filenam');
-    //     const val = url.searchParams.get('value');
+    const fil = url.searchParams.get('filenam');
+    const val = url.searchParams.get('value');
 
-    //     fs.writeFileSync(`src/lib/${fil}.txt`, val);
+    fs.writeFileSync(`src/lib/${fil}.txt`, val);
 
 
 
