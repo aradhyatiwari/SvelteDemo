ca351605e871a3a215143c81be09d3b7bc9c4036
Aradhya Tiwari <aradhya.prad@gmail.com>
2022-10-26 14:37:47 +0530

    commit made to be undo

--- src/routes/api/+server.js
+++ src/routes/api/+server.js
@@ -1,13 +1,13 @@
 //import * as fs from 'fs'
-import * as fs from 'fs';
+// import * as fs from 'fs';
 
 export function GET({ url }) {
-    const fil = url.searchParams.get('filenam');
-    const val = url.searchParams.get('value');
+    //     const fil = url.searchParams.get('filenam');
+    //     const val = url.searchParams.get('value');
 
-    fs.writeFileSync(`src/lib/${fil}.txt`, val);
+    //     fs.writeFileSync(`src/lib/${fil}.txt`, val);
 
 
 
-    return new Response(String(val));
+    return new Response(String("value is returned"));
 }
