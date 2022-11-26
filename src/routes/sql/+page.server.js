7d368ec71e7c61fe09e68da0c1357373e1887a17
Aradhya Tiwari <aradhya.prad@gmail.com>
2022-11-26 23:48:31 +0530

    ss

--- src/routes/sql/+page.server.js
+++ src/routes/sql/+page.server.js
@@ -1,47 +1,47 @@
-// // @ts-nocheck
-// export const prerender = true;
-// import PrismaClientPkg from "@prisma/client"
-// const PrismaClient = PrismaClientPkg.PrismaClient;
-// const prisma = new PrismaClient()
-// let result;
+// @ts-nocheck
+export const prerender = true;
+import PrismaClientPkg from "@prisma/client"
+const PrismaClient = PrismaClientPkg.PrismaClient;
+const prisma = new PrismaClient()
+let result;
 
-// export function load() {
-//     async function main() {
+export function load() {
+    async function main() {
 
-//         // await prisma.user.create({
-//         //     data: {
-//         //         name: 'Alice',
-//         //         email: 'alice@prisma.io',
-//         //         posts: {
-//         //             create: { title: 'Hello World' },
-//         //         },
-//         //         profile: {
-//         //             create: { bio: 'I like turtles' },
-//         //         },
-//         //     },
-//         // })
+        // await prisma.user.create({
+        //     data: {
+        //         name: 'Alice',
+        //         email: 'alice@prisma.io',
+        //         posts: {
+        //             create: { title: 'Hello World' },
+        //         },
+        //         profile: {
+        //             create: { bio: 'I like turtles' },
+        //         },
+        //     },
+        // })
 
-//         const allUsers = await prisma.user.findMany({
+        const allUsers = await prisma.user.findMany({
 
-//             include: {
-//                 posts: true,
-//                 profile: true,
-//             },
-//         })
+            include: {
+                posts: true,
+                profile: true,
+            },
+        })
 
-//         console.dir(allUsers, { depth: null })
-//         result = allUsers;
-//     }
+        console.dir(allUsers, { depth: null })
+        result = allUsers;
+    }
 
-//     main()
-//         .then(async () => {
-//             await prisma.$disconnect()
-//         })
-//         .catch(async (e) => {
-//             console.error(e + "Some error occured")
-//             await prisma.$disconnect()
-//             process.exit(1)
-//         })
+    main()
+        .then(async () => {
+            await prisma.$disconnect()
+        })
+        .catch(async (e) => {
+            console.error(e + "Some error occured")
+            await prisma.$disconnect()
+            process.exit(1)
+        })
 
-//     return { result };
-// }
+    return { result };
+}
